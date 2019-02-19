70bce963ef9638ca5b40f828ebaf6f349413bc28
Carles Mateu <carlesm@carlesm.com>
2019-02-19 11:46:36 +0100

    Downloads and prints activites list

--- webclient.py
+++ webclient.py
@@ -3,13 +3,14 @@
 # vim: set fileencoding=utf8 :
 
 from urllib.request import urlopen
+import bs4
 
 class WebClient(object):
     """WebClient class"""
     def __init__(self):
         super(WebClient, self).__init__()
 
-    def download_page(arg):
+    def download_page(self):
         # connect to the web site
         f = urlopen("http://www.eps.udl.cat/ca/")
         # get the download_page
@@ -18,12 +19,23 @@ class WebClient(object):
         f.close()
         return page
 
+    def search_activities(self, page):
+        tree = bs4.BeautifulSoup(page,"lxml")
+        activities = tree.find_all("div","featured-links-item")
+        act_list = []
+        for activity in activities:
+            title = activity.find("span","flink-title")
+            link = activity.find("a")
+            act_list.append((title.text, link["href"]))
+        return act_list
+
     def run(self):
         # download a web page
         page = self.download_page()
         # search activities in web page
+        data = self.search_activities(page)
         # print the activities
-        print(page)
+        print(data)
 
 if __name__ == "__main__":
     c = WebClient()
